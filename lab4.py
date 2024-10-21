dbffdfd24f0e2a71e29b27c8cd04701c4cbe8a84
cole-hackman <hackman@calpoly.edu>
2024-10-21 16:51:48 -0700

    Implemented functions and added unit tests to verify the functionality of each function and make sure there are accurate results
    - Added first_element(inputList) to return the first element of each non-empty nested list.
    - Implemented x_coordinates(inputList) to extract the x-coordinates from a list of Point objects.
    - Added are_in_positive_quadrant(inputList) to return points in the positive quadrant (where x > 0 and y > 0).
    - Implemented distance(point1, point2) to calculate Euclidean distance between two points.
    - Added manhattan_distance(point1, point2) to calculate the Manhattan distance between two points.
    - Implemented distance_all(inputList) to calculate the Manhattan distance of a list of points from the origin

--- lab4.py
+++ lab4.py
@@ -1,22 +1,45 @@
 import data
+import math
+from data import Point
 
 # Write your functions for each part in the space below.
 
 # Part 1
-
+def first_element(inputList: list[list[int]]) -> list:
+    newList = []
+    for sublist in inputList:
+        if len(sublist) > 0:
+            newList.append(sublist[0])
+    return newList
 
 # Part 2
-
+def x_coordinates(inputList:list[Point]) -> list:
+    xList = []
+    for points in inputList:
+        xList.append(points.x)
+    return xList
 
 # Part 3
-
-
+def are_in_positive_quadrant(inputList:list[Point]) -> list:
+    pos_coord_list = []
+    for points in inputList:
+        if points.x > 0 and points.y > 0:
+            pos_coord_list.append(points)
+    return pos_coord_list
 # Part 4
-
+def distance(point1: Point, point2: Point) -> float:
+    euclidean = math.sqrt(((point2.x - point1.x) ** 2) + ((point2.y - point1.y) ** 2))
+    return euclidean
 
 # Part 5
-
+def manhattan_distance(point1: Point, point2: Point) -> float:
+    manhattan = (abs(point1.x - point2.x) + abs(point1.y - point2.y))
+    return manhattan
 
 # Part 6
-
-
+def distance_all(inputList: list[Point]) -> list:
+    origin = Point(0,0)
+    distanceList = []
+    for point in inputList:
+        distanceList.append(manhattan_distance(point, origin))
+    return distanceList
